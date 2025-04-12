7702be1782698095bc718711db03278c514ae74e
Lev Vedrov <102747673+zipo0@users.noreply.github.com>
2025-04-12 18:34:45 +0300

    Update Invoke-ConPtyShell.ps1

--- Invoke-ConPtyShell.ps1
+++ Invoke-ConPtyShell.ps1
@@ -637,7 +637,7 @@ ${esc}[31m
     \ \  \_|  \ \  \_|\ \ \  \ \  \ \  \____
      \ \__\    \ \_______\ \__\ \__\ \_______\
       \|__|     \|_______|\|__|\|__|\|_______|
-                BALLS
+                GLO GLO
 ${esc}[0m
 
 ${esc}[32m[+] Connected :: $env:USERNAME@$env:COMPUTERNAME
